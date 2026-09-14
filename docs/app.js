3f20a8870317a4903b024d5440d0300648f731f8
root <michaelmofaka@gmail.com>
2026-09-14 16:38:42 +0300

    updated

---
+++ docs/app.js
@@ -0,0 +1,31 @@
+function order(product) {
+
+    const phone = "254700000000";
+
+    const message =
+        `Hello, I would like to order ${product}.`;
+
+    const url =
+        `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
+    window.open(url, "_blank");
+}
+async function shareBusiness() {
+
+    if (navigator.share) {
+
+        await navigator.share({
+            title: "Mama Nakuru",
+            text: "Check out Mama Nakuru",
+            url: window.location.href
+        });
+
+    } else {
+
+        await navigator.clipboard.writeText(
+            window.location.href
+        );
+
+        alert("Business link copied!");
+    }
+}
